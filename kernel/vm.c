1059c89cc6cf442dbf646cffbf094562279c1e14
richardlzx <1976370188@qq.com>
2022-12-31 17:18:22 +0800

    完成了第一个作业

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+_vmprint(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      if(level == 0)
+        printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      else if(level == 1)
+        printf(".. ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      uint64 child = PTE2PA(pte);
+      _vmprint((pagetable_t)child, level + 1);
+    } else if(pte & PTE_V){
+        printf(".. .. ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 0);
+}
